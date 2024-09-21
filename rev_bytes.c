1ea0230efafe14f1382299c75c793588d04ab060
srjshinde <srjshinde@gmail.com>
2024-09-21 11:11:56 +0530

    Add codes for byte swapping

---
+++ rev_bytes.c
@@ -0,0 +1,15 @@
+/* C program to reverse the bytes */
+
+#include<stdio.h>
+
+unsigned a = 0xAABBCCDD;
+
+int main(int argc, char *argv[])
+{
+
+    a = ((a & 0x000000FF)<<24) | ((a & 0x0000FF00)<<8) | ((a & 0x00FF0000)>>8) | ((a & 0xFF000000)>>24);
+
+    printf("Reversed order is %x\n", a);
+
+    return 0;
+}
---
+++ rev_bytes_bswap.c
@@ -0,0 +1,15 @@
+/* C program to reverse the bytes */
+
+#include<stdio.h>
+#include<byteswap.h>
+#include<inttypes.h>
+
+uint32_t a = 0xAABBCCDD;
+
+int main(int argc, char *argv[])
+{
+    a = bswap_32(a);
+    printf("Reversed order is %x\n", a);
+
+    return 0;
+}
